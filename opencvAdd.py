49a21b91d98abab07844c8debc313675c9384b46
chiahaochang <zhzhzhxx95@gmail.com>
2021-07-27 09:58:32 +0800

    opencv add練習2

--- opencvAdd.py
+++ opencvAdd.py
@@ -12,7 +12,7 @@ print(len(img1),len(img1[0]))
 roi1 = img1[150:300,600:700]
 roi2 = img1[50:200,200:300]
 
-dst = cv.addWeighted(roi1,0.7,roi2,0.3,0)
+dst = cv.addWeighted(roi1,0.5,roi2,0.5,0)
 img1[50:200,200:300] = dst
 
 cv.imshow('show',img1)
