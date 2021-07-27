00cd38f82070b63f04897b9e4fdc20d14b026023
chiahaochang <zhzhzhxx95@gmail.com>
2021-07-27 17:25:06 +0800

    濾波

--- filter.py
+++ filter.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('./img/parie.jpg')
+img = cv2.imread('./img/rem.png')
 
 kernel = np.ones((5, 5), np.float32)/25  # 创建一个5x5的平均滤波器核
+# 2D卷积滤波
 dst = cv2.filter2D(img, -1, kernel)
 
 # 平均模糊
@@ -25,6 +26,7 @@ plt.subplot(233), plt.imshow(blur), plt.title('Averaging')
 plt.subplot(234), plt.imshow(gauss), plt.title('GuassianBlur')
 plt.subplot(235), plt.imshow(median), plt.title('MedianBlur')
 plt.subplot(236), plt.imshow(bila), plt.title('BilateralFilter')
-
-
+print(img.shape)
+image = cv2.resize(img, (600, 800), interpolation=cv2.INTER_AREA)
+cv2.imshow("rem",image)
 plt.show()
