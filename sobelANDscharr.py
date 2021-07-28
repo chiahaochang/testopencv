31f2bd0ae29868cf257de8ac283921e29123d16e
chiahaochang <zhzhzhxx95@gmail.com>
2021-07-28 09:58:43 +0800

    scharr add

--- sobelANDscharr.py
+++ sobelANDscharr.py
@@ -4,14 +4,12 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('./img/car.jpg', 0)
 
-sobel = cv2.Sobel(img,cv2.CV_16U, 1, 1, ksize = 1)
-sobel5 = cv2.Sobel(img,cv2.CV_16U, 1, 1,ksize= 5)
-sobelx = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize = 5)
-sobely = cv2.Sobel(img, cv2.CV_8U, 0, 1, ksize = 5)
+scharr = cv2.Scharr(img,cv2.CV_8U, 0, 1)
+scharrx = cv2.Scharr(img,cv2.CV_8U, 1, 0)
+scharry = cv2.Scharr(img,cv2.CV_8U, 0, 1)
 
-plt.subplot(221), plt.imshow(sobelx, cmap='gray'), plt.title('sobelx')
-plt.subplot(222), plt.imshow(sobely, cmap='gray'), plt.title('sobely')
-plt.subplot(223), plt.imshow(sobel, cmap='gray'), plt.title('ksize 3')
-plt.subplot(224), plt.imshow(sobel, cmap='gray'), plt.title('ksize 5')
+plt.subplot(221), plt.imshow(scharr, cmap='gray'), plt.title('scharr')
+plt.subplot(222), plt.imshow(scharrx, cmap='gray'), plt.title('scharr x')
+plt.subplot(223), plt.imshow(scharry, cmap='gray'), plt.title('scharr y')
 
 plt.show()
