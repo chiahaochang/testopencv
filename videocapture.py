b55e7760f40cfe57bec5e7f820b3947dd0752c51
chiahaochang <zhzhzhxx95@gmail.com>
2021-07-30 11:55:45 +0800

    730

--- videocapture.py
+++ videocapture.py
@@ -5,9 +5,9 @@ while(True):
     # 一帧一帧捕捉
     ret, frame = cap.read()
     # 我们对帧的操作在这里
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # 显示返回的每帧
-    cv.imshow('frame',gray)
+    cv.imshow('frame',frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 # 当所有事完成，释放 VideoCapture 对象
